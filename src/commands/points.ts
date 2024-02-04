72629fa7905b96e924cf73ba2f7364d966006080
Fernando Ramirez <ramirez.fernando2003@gmail.com>
2024-02-03 21:32:55 -0600

    refactor: add subcommands to points (#9)
    * refactor: add subcommands to points
    
    * fix: handle proper is member validation

--- src/commands/points.ts
+++ src/commands/points.ts
@@ -1,9 +1,8 @@
 
 import { 
     SlashCommandBuilder,
-    SlashCommandMentionableOption,
+    ChatInputCommandInteraction,
     GuildMember,
-    SlashCommandStringOption,
 } from "discord.js";
 import { CoCommand } from "../structures";
 import { embedSuccess, embedError } from "../constants/embeds";
@@ -20,131 +19,121 @@ const Points = new CoCommand({
     data: new SlashCommandBuilder()
         .setName("points")
         .setDescription("View member points!")
-        .addMentionableOption((option: SlashCommandMentionableOption) => 
-            option
-                .setName("mention")
-                .setDescription("Mention a discord user to view their member points!")
-                .setRequired(false))
-        .addStringOption((option: SlashCommandStringOption) => 
-            option
-                .setName("input")
-                .setDescription("View the points leaderboard by passing in 'lb' to the input!")
-                .setRequired(false)),
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("view")
+                .setDescription("View points of yourself or a mentioned user")
+                .addMentionableOption(option => 
+                    option.setName("mention")
+                        .setDescription("Mention a discord user")
+                        .setRequired(false)))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName("leaderboard")
+                .setDescription("View the points leaderboard")),
     execute: async ({ interaction })=> {
-        try {
-            await interaction.deferReply({ ephemeral: false });
+        await interaction.deferReply({ ephemeral: false });
 
-            const mentionOptionChose = interaction.options.get("mention")
-            const inputOptionChose = interaction.options.get("input")
-            if (mentionOptionChose && inputOptionChose) {
-                const embed = embedError("Only choose one option, either 'mention' or 'input'")
-                interaction.editReply({ embeds: [embed] });
-                return
-            }
-            else if (inputOptionChose) {
-                const inputField = interaction.options.get("input")?.value
-                if (inputField != 'lb') {
-                    const embed = embedError("The only choices allowed in the input are: 'lb'")
-                    interaction.editReply({ embeds: [embed] });
-                    return
-                }
-                const amount = 10;
-                const url = process.env.POINTS_LEADERBOARD_API_ENDPOINT + `?top=${amount}`
-                const options = {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                }
-                fetch(url, options)
-                    .then(res => {
-                        return res.json()
-                    })
-                    .then(data => {
-                        if (data.success) {
-                            const embed = embedSuccess('Code[Coogs] Points Leaderboard', 'View the top 10 members with most points!')
-                            data.data.forEach((entry: any, index: number) => {
-                                const rank = (index + 1).toString();
-                                embed.addFields(
-                                    { name: `#${rank} - ${entry.first_name} ${entry.last_name}`, value: `${entry.points} points` }
-                                );
-                            });
+        const subCommand = interaction.options.getSubcommand();
 
-                            interaction.editReply({ embeds: [embed] });
-                            return
-                        }
-                        const embed = embedError(data.error.message)
-                        interaction.editReply({ embeds: [embed] });
-                    })
-                    .catch(error => {
-                        const embed = embedError(error.toString())
-                        interaction.editReply({ embeds: [embed] });
-                    })
-            }
-            else if (mentionOptionChose || (!inputOptionChose && !mentionOptionChose)) {
-                const roleName = interaction.options.get("mention")?.role?.name
-                const mentionedEveryoneOrHere = (roleName == "@everyone") || (roleName == "@here")
-                if (mentionedEveryoneOrHere) {
-                    const embed = embedError("Don't mention @everyone or @here")
-                    interaction.editReply({ embeds: [embed] });
-                    return
-                }
-
-                const mentionedUser = interaction.options.get("mention")?.member as GuildMember;
-                if (mentionedUser) {
-                    const mentionedUserHasMemberRole = mentionedUser.roles.cache.some(role => role.name === 'member');
-                    if (!mentionedUserHasMemberRole) {
-                        const embed = embedError("The user you mentioned is not verified")
-                        interaction.editReply({ embeds: [embed] });
-                        return 
-                    }
-                }
-                else {
-                    const user = interaction.member as GuildMember;
-                    const hasMemberRole = user.roles.cache.some(role => role.name === 'member');            
-                    if (!hasMemberRole) {
-                        const embed = embedError("You are not verified, use the '/verify' command to verify.")
-                        interaction.editReply({ embeds: [embed] });
-                        return 
-                    }
-                }   
-
-                const discordId = mentionedUser ? mentionedUser.id : interaction.user.id;
-
-                const url = process.env.POINTS_API_ENDPOINT + `?discordId=${discordId}`
-                const options = {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                }
-                fetch(url, options)
-                    .then(res => {
-                        return res.json()
-                    })
-                    .then(data => {
-                        if (data.success) {
-                            const embed = embedSuccess('Code[Coogs] User Points', 'View a users points!')
-                            embed.addFields(
-                                { name: `${data.data.first_name} ${data.data.last_name}`, value: `${data.data.points} points` }
-                            );
-
-                            interaction.editReply({ embeds: [embed] });
-                            return
-                        }
-                        const embed = embedError(data.error.message)
-                        interaction.editReply({ embeds: [embed] });
-                    })
-                    .catch(error => {
-                        const embed = embedError(error.toString())
-                        interaction.editReply({ embeds: [embed] });
-                    })
-            }
-        } catch (error) {
-            const embed = embedError(`${error}`)
-            interaction.editReply({ embeds: [embed] });
+        switch(subCommand) {
+            case 'view':
+                handleViewPoints(interaction)
+                break;
+            case 'leaderboard':
+                handleLeaderboard(interaction);
+                break;
         }
     }
 });
 
 export default Points;
+
+const isMember = (user: GuildMember) => {
+    return user.roles.cache.some(role => role.name === 'member');             
+}
+
+const handleViewPoints = (interaction: ChatInputCommandInteraction) => {
+    const mentionedUser = interaction.options.get("mention")?.member as GuildMember;
+    if (mentionedUser) {
+        if (!isMember(mentionedUser)) {
+            const embed = embedError("The user you mentioned is not verified.")
+            interaction.editReply({ embeds: [embed] });
+            return 
+        }
+    } 
+    else {
+        const user = interaction.member as GuildMember;
+        if (!isMember(user)) {
+            const embed = embedError("You are not verified, use the '/verify' command to verify.")
+            interaction.editReply({ embeds: [embed] });
+            return 
+        }
+    }
+
+    const discordId = mentionedUser ? mentionedUser.id : interaction.user.id;
+
+    const url = process.env.POINTS_API_ENDPOINT + `?discordId=${discordId}`
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    fetch(url, options)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            if (data.success) {
+                const embed = embedSuccess('Code[Coogs] User Points', 'View a users points!')
+                embed.addFields(
+                    { name: `${data.data.first_name} ${data.data.last_name}`, value: `${data.data.points} points` }
+                );
+
+                interaction.editReply({ embeds: [embed] });
+                return
+            }
+            const embed = embedError(data.error.message)
+            interaction.editReply({ embeds: [embed] });
+        })
+        .catch(error => {
+            const embed = embedError(error.toString())
+            interaction.editReply({ embeds: [embed] });
+        })
+}
+
+const handleLeaderboard = (interaction: ChatInputCommandInteraction) => {
+    const amount = 10;
+    const url = process.env.POINTS_LEADERBOARD_API_ENDPOINT + `?top=${amount}`
+    const options = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    fetch(url, options)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            if (data.success) {
+                const embed = embedSuccess('Code[Coogs] Points Leaderboard', 'View the top 10 members with most points!')
+                data.data.forEach((entry: any, index: number) => {
+                    const rank = (index + 1).toString();
+                    embed.addFields(
+                        { name: `#${rank} - ${entry.first_name} ${entry.last_name}`, value: `${entry.points} points` }
+                    );
+                });
+
+                interaction.editReply({ embeds: [embed] });
+                return
+            }
+            const embed = embedError(data.error.message)
+            interaction.editReply({ embeds: [embed] });
+        })
+        .catch(error => {
+            const embed = embedError(error.toString())
+            interaction.editReply({ embeds: [embed] });
+        })
+}
